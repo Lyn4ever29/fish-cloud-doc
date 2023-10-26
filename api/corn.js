b03e25bd374c0a049d953bad5da16c8bb9517259
Lyn4ever29 <Lyn4ever29@163.com>
2023-10-26 10:08:23 +0800

    build

--- api/corn.js
+++ api/corn.js
@@ -10,7 +10,7 @@ const matter = require('gray-matter'); // FrontMatter解析器 https://github.co
 const readFileList = require('./modules/readFileList');
 const urlsRoot = path.join(__dirname, '..', 'urls.txt'); // 百度链接推送文件
 // const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
-const DOMAIN = "http://cloud.jhacker.cn"
+const DOMAIN = "https://fishcloud.top"
 if (!DOMAIN) {
     logger.error('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://xugaoyi.com')
     return
